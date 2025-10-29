f104157c0d081466ee4bb78a1ab815de4f440897
Lars Nitzschke <lars.nitzschke@tu-dortmund.de>
2025-10-29 16:51:39 +0100

    aquaify constant arrays

--- src/main/kotlin/tools/aqua/konstraints/solvers/z3/Z3ExpressionConverter.kt
+++ src/main/kotlin/tools/aqua/konstraints/solvers/z3/Z3ExpressionConverter.kt
@@ -110,14 +110,7 @@ fun Expr<Z3ArraySort<*, *>>.aquaify(): Expression<ArraySort> =
           this.args[1].aquaify(),
           this.args[2].aquaify())
     } else if (isConstantArray) {
-      println("Check this: $this")
       ConstantArray(this.sort.aquaify() as ArraySort, this.args[0].aquaify())
-      // } else if (isSelect) {
-      //    ArraySelect(
-      //        this.args[0].aquaify() as Expression<ArraySort>,
-      //        this.args[1].aquaify())
     } else {
-      throw RuntimeException(
-          "Unknown or unsupported array expression $this") // constant array, default array,
-      // relation
+      throw RuntimeException("Unknown or unsupported array expression $this")
     }
